f868b3d65d113becf57a746354ab3842da0cdc10
Nancy Fan <NancyFan@appledemacbook-air.local>
2016-05-23 22:56:02 +0800

    Initial Commit

---
+++ SpeakerRecognition.3.0/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SpeakerRecognition.3.0
+//
+//  Created by Nancy Fan on 16/5/23.
+//  Copyright © 2016年 Nancy Fan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
